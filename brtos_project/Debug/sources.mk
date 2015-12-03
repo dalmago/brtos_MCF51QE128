6ce316f0bc5e1f99a391821debf9a2471cdd2408
Matheus <matheusdalmago10@hotmail.com>
2015-12-03 13:25:39 -0200

    adding brtos eclipse project

---
+++ brtos_project/Debug/sources.mk
@@ -0,0 +1,29 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+ELF_SRCS := 
+OBJ_SRCS := 
+OUT_SRCS := 
+EXE_SRCS := 
+ASM_SRCS := 
+C_SRCS := 
+S_UPPER_SRCS := 
+O_SRCS := 
+SIZE_FILE_OUTPUTS := 
+EXECUTABLES := 
+LIST_FILE_OUTPUTS := 
+OBJS := 
+ARCHIVES := 
+MAP_FILE_OUTPUTS := 
+SYMBOL_LIST_FILE_OUTPUTS := 
+C_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+Source_MemoryAllocation \
+Sources \
+Sources_brtos \
+Sources_hal \
+Startup_Code \
+
